fa1c3a32394ac35017cc665506d1ac51b55bf219
SabuCay <cahl@kea.dk>
2024-10-21 09:46:38 +0200

    CSS demo

---
+++ src/main/java/com/example/mycssdemo/MyCssDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mycssdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCssDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCssDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mycssdemo/MyCssDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mycssdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCssDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
